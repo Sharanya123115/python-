3f9783d1455b35910f6174a201c8feca1dd880e2
Sharanya123115 <korakandlasharanya@gmail.com>
2025-05-31 12:03:06 +0530

    Add files via upload

---
+++ longet_substring_without_repeating_characters(optimal sol).py
@@ -0,0 +1,18 @@
+def Substring(s):
+    n=len(s)
+    maxLen=0
+    left=0
+    right=0
+    d={}
+    while(right<n):
+        if(s[right] in d):
+            if(d[s[right]]>=left):
+                left=d[s[right]]+1
+        d[s[right]]=right
+        maxLen=max(maxLen,right-left+1)
+        right+=1
+    return maxLen
+s=input()
+print(Substring(s))
+#s:abcabcccb
+#output:3'''
